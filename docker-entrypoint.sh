18f09d0aeb38d1e396313ff4f2be572d7234a2a5
c0tt0n-candy <dear-spring@excite.co.jp>
2016-11-06 15:08:06 +0900

    modify to create database in PostgreSQL https://github.com/Dragontek/octobercms/blob/master/fpm/docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -135,38 +135,32 @@ EOPHP
 		set_config 'WP_DEBUG' 1 boolean
 	fi
 
-	TERM=dumb php -- "$WORDPRESS_DB_HOST" "$WORDPRESS_DB_USER" "$WORDPRESS_DB_PASSWORD" "$WORDPRESS_DB_NAME" <<'EOPHP'
+	TERM=dumb php -- "$WORDPRESS_DB_HOST" "$POSTGRES_PORT_5432_TCP" "$WORDPRESS_DB_USER" "$WORDPRESS_DB_PASSWORD" "$WORDPRESS_DB_NAME" <<'EOPHP'
 <?php
 // database might not exist, so let's try creating it (just to be safe)
-$stderr = fopen('php://stderr', 'w');
-// https://codex.wordpress.org/Editing_wp-config.php#MySQL_Alternate_Port
-//   "hostname:port"
-// https://codex.wordpress.org/Editing_wp-config.php#MySQL_Sockets_or_Pipes
-//   "hostname:unix-socket-path"
-list($host, $socket) = explode(':', $argv[1], 2);
-$port = 0;
-if (is_numeric($socket)) {
-	$port = (int) $socket;
-	$socket = null;
-}
+$host=$argv[1];
+$port=$argv[2];
+$dbuser=$argv[3];
+$dbpass=$argv[4];
+$dbname=$argv[5];
 $maxTries = 10;
-do {
-	$mysql = new mysqli($host, $argv[2], $argv[3], '', $port, $socket);
-	if ($mysql->connect_error) {
-		fwrite($stderr, "\n" . 'MySQL Connection Error: (' . $mysql->connect_errno . ') ' . $mysql->connect_error . "\n");
-		--$maxTries;
-		if ($maxTries <= 0) {
-			exit(1);
-		}
+
+while ($maxTries > 0)
+{
+	try {
+		$pdo = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $dbuser, $dbpass);
+		// Postgres version of "CREATE DATABASE IF NOT EXISTS"
+		$res = $pdo->query("select count(*) from pg_catalog.pg_database where datname = '$dbname';");
+		if($res->fetchColumn() < 1)
+			$pdo->query("CREATE DATABASE $dbname");
+
+		$maxTries = 0;
+	} catch (PDOException $e) {
+		echo $e;
+		$maxTries--;
 		sleep(3);
 	}
-} while ($mysql->connect_error);
-if (!$mysql->query('CREATE DATABASE IF NOT EXISTS `' . $mysql->real_escape_string($argv[4]) . '`')) {
-	fwrite($stderr, "\n" . 'MySQL "CREATE DATABASE" Error: ' . $mysql->error . "\n");
-	$mysql->close();
-	exit(1);
 }
-$mysql->close();
 EOPHP
 fi
 
